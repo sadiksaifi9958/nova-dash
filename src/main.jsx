0a3cafb0668db77ce79449fd9b139687f12d0aab
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-06-19 15:17:00 +0530

    Add Zustand sidebar store and wrap app with TooltipProvider and AppProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AppProvider } from "./context/AppContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </TooltipProvider>
   </StrictMode>,
-)
+);
--- src/store/useSidebarStore.js
+++ src/store/useSidebarStore.js
@@ -1,5 +1,7 @@
-function useSidebarStore() {
-  return null;
-}
-
-export default useSidebarStore;
+import { create } from "zustand";
+export const useSidebarStore = create((set) => ({
+  isOpen: true,
+  toggleSidebar: () => set((state) => ({ isOpen: !state.isOpen })),
+  closeSidebar: () => set({ isOpen: false }),
+  openSidebar: () => set({ isOpen: true }),
+}));
